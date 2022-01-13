7cb3da17a1cb062e5ac7cf962f5a6b52b3df7a79
EC2 Default User <ec2-user@ip-172-31-95-58.ec2.internal>
2022-01-13 14:58:20 +0000

    [Add]Gemfileの追加、各モデル、テーブルとカラムの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/show'
+  devise_for :customers
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  get "home/about" => "homes#about", as:"about"
+  
 end
